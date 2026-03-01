c6dd2b763234ff4d159d2f82015ac757cfa7205c
lemon1487 <13479393774@163.com>
2026-03-01 14:01:07 +0800

    更新管理员功能函数，补全功能

--- include/admin_func.h
+++ include/admin_func.h
@@ -3,16 +3,18 @@
 
 #include "common.h"
 
-// 管理员功能函数声明
-void admin_menu(Student* stu_list, int* stu_count, User* user_list, int* user_count,
-    Appeal* appeal_list, int* appeal_count, Todo* todo_list, int* todo_count);// 管理员主菜单入口
-void register_user(User* user_list, int* user_count); // 主界面注册功能（新增用户账号）
-void manage_user(User* user_list, int* user_count);    // 管理所有用户账号（增/删/查）
-void manage_todo(Todo* todo_list, int* todo_count);    // 管理待办事项（查看/标记完成/删除）
-void manage_appeal(Appeal* appeal_list, int* appeal_count, User* user_list, int user_count);// 处理学生申诉（成绩/密码找回）
-void admin_change_pwd(User* user_list, int user_count); // 管理员修改自身密码
+// 管理员主菜单
+void admin_menu(StudentNode* stu_head, int stu_count, UserNode* user_head, int user_count,
+    Appeal* appeal_list, int appeal_count, Todo* todo_list, int todo_count);
+// 管理员修改密码
+void admin_change_pwd(UserNode* head, int user_count);
+// 用户管理
+void manage_user(UserNode** head, int* count);
+// 申诉管理
+void manage_appeal(Appeal* appeal_list, int* appeal_count, UserNode** user_head, int* user_count);
+// 待办管理
+void manage_todo(Todo* todo_list, int* todo_count);
+// 学生信息管理
+void manage_student(StudentNode** stu_head, int* stu_count);
 
-// 密码找回相关声明
-void submit_find_pwd_appeal(Appeal* appeal_list, int* appeal_count, Todo* todo_list, int* todo_count, char* account);// 提交密码找回申诉
-
-#endif
+#endif // ADMIN_FUNC_H
