de3433141eee50d2135610334d885bb3c0fec3df
lemon1487 <13479393774@163.com>
2026-03-01 13:58:51 +0800

    更新学生功能函数说明，更加配对学生身份

--- include/student_func.h
+++ include/student_func.h
@@ -2,14 +2,18 @@
 #define STUDENT_FUNC_H
 
 #include "common.h"
+//学生菜单展示
+void student_menu(StudentNode* stu_head, int stu_count, UserNode* user_head, int user_count, char* account,
+    Appeal* appeal_list, int* appeal_count, Todo* todo_list, int* todo_count);
+//个人成绩
+void student_view_score(StudentNode* stu_head, int stu_count, char* account);
+//学生成绩分析
+void student_score_analysis(StudentNode* stu_head, int stu_count, char* account);
+//成绩申诉
+void student_submit_appeal(Appeal* appeal_list, int* appeal_count, Todo* todo_list, int* todo_count, char* account);
+//密码修改
+void student_change_pwd(UserNode* user_head, int user_count, char* account);
+//查看本班成绩
+void student_view_class_score(StudentNode* stu_head, int stu_count, char* account);
 
-// 学生功能函数声明
-void student_menu(Student* stu_list, int stu_count, User* user_list, int user_count,
-    char* account, Appeal* appeal_list, int* appeal_count,
-    Todo* todo_list, int* todo_count);// 学生主菜单入口
-void view_my_score(Student* stu_list, int stu_count, char* account); // 查看个人成绩信息
-void view_class_score(Student* stu_list, int stu_count, char* account); // 查看本班成绩信息
-void score_analysis(Student* stu_list, int stu_count, char* account); // 查看本班成绩排名分析
-void student_change_pwd(User* user_list, int user_count, char* account); // 学生修改自身密码
-
-#endif
+#endif // STUDENT_FUNC_H
